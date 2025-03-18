3488144789c39d762cefde742031c122b29099bf
eLieZERelIshA89614 <mutagen.vetches-0y@icloud.com>
2025-03-18 10:03:03 +0100

    Implement a search functionality in the file

--- 4cs/YBY/JrG/distance-education_idsbMd_1741573159.md.js
+++ 4cs/YBY/JrG/distance-education_idsbMd_1741573159.md.js
@@ -106,7 +106,6 @@ module.exports = function(grunt) {
             style: 'compressed'
           },
           files: {
-            'assets/css/main.css': 'assets/sass/main.scss'
           }
         },
         dev: {
